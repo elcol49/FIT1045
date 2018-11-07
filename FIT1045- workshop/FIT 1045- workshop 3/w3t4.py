668cda0a791e1a31e638ae77938cef799c256880
megan21ooi <40974140+megan21ooi@users.noreply.github.com>
2018-11-07 18:46:10 +0800

    python table

--- FIT1045- workshop/FIT 1045- workshop 3/w3t4.py
+++ FIT1045- workshop/FIT 1045- workshop 3/w3t4.py
@@ -1,3 +1,20 @@
+#method 1- according to question
+
+list2 = [0]*5
+
+for i in range(5):
+    x = input("please enter numbers with space: ")
+    y = x.split(' ')
+    list2[i] = [0]*len(y)
+
+    for j in range(len(y)):
+        #int- to change from string to int
+        #y[j] means element in list y index j
+        list2[i][j] = int(y[j])
+        
+print(list2)    
+
+#method 2- using append method
 list1 = []
 count = 0
 
@@ -13,9 +30,9 @@ while count < 5:
 
 print(list1)
 
-#task 4 discussion not completed
-"""
-method 2
+
+#method 3- using list(input()) but if input 1111 it means 1,1,1,1 so cannot input
+#numbers like 13 because it'll be shown as 1,3
 
 list2 = []
 
@@ -27,4 +44,5 @@ while count2 < 5:
     list2.append(x)
     count2 += 1
 
-print(list2)"""
+print(list2)
+
